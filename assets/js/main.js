55980b13f46fdbeb2d3077c4df97c1801ab7d0b2
Deven Pattni <devpatt@users.noreply.github.com>
2018-01-15 17:58:31 -0500

    content update

--- assets/js/main.js
+++ assets/js/main.js
@@ -90,7 +90,7 @@ jQuery(document).ready(function ($) {
 
 
     /*---------------------------------------------*
-     * Counter 
+     * Counter
      ---------------------------------------------*/
 
 //    $('.statistic-counter').counterUp({
